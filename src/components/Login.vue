26e0254f67468b0ee1d186aee067c0a2e177821f
ss <3053767521@qq.com>
2019-05-18 18:39:02 +0800

    modify comment

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,13 +6,16 @@
           <img class="mx-auto d-block" src="../assets/register.jpg" alt="">
           <form @submit.prevent="onSubmit">
             <div class="form-group">
-              <label for="email">邮箱</label>
+              <!-- email -->
+              <label for="email">邮箱：</label>
               <input type="email" class="form-control" v-model="email">
             </div>
             <div class="form-group">
-              <label for="password">密码</label>
+              <!-- password -->
+              <label for="password">密码：</label>
               <input type="password" class="form-control" v-model="password">
             </div>
+            <!-- 提交 -->
             <button type="submit" class="btn btn-block btn-success">登录</button>
           </form>
         </div>
