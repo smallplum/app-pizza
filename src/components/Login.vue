631bc5f372f68acbb6f23d5c8527d7f5666e8a44
ss <3053767521@qq.com>
2019-05-18 19:11:14 +0800

    ’修改bug‘

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,7 @@
           <img class="mx-auto d-block" src="../assets/register.jpg" alt="">
           <form @submit.prevent="onSubmit">
             <div class="form-group">
-              <!-- email -->
+              <!-- email@qq.com -->
               <label for="email">邮箱：</label>
               <input type="email" class="form-control" v-model="email">
             </div>
@@ -15,7 +15,7 @@
               <label for="password">密码：</label>
               <input type="password" class="form-control" v-model="password">
             </div>
-            <!-- 提交 -->
+        <!-- login -->
             <button type="submit" class="btn btn-block btn-success">登录</button>
           </form>
         </div>
